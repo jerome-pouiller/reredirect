e46794a227b65e22de5fa41338eb1b1bd619776c
Jérôme Pouiller <jezz@sysmic.org>
2014-08-26 10:13:27 +0200

    Add copyright in revelant files

--- attach.c
+++ attach.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2011 by Nelson Elhage
+ * Copyright (C) 2014 by Jérôme Pouiller <jezz@sysmic.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
